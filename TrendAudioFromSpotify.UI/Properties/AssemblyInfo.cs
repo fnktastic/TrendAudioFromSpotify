633ff17fd59c1d5ecbfa8a6732dff13fe26a3026
Oleksandr Herhelezhyu <olek.h@outlook.com>
2020-04-08 15:13:58 +0300

    Version update

--- TrendAudioFromSpotify.UI/Properties/AssemblyInfo.cs
+++ TrendAudioFromSpotify.UI/Properties/AssemblyInfo.cs
@@ -49,5 +49,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.00.000")]
-[assembly: AssemblyFileVersion("1.00.000")]
+[assembly: AssemblyVersion("1.10.005")]
+[assembly: AssemblyFileVersion("1.10.005")]
